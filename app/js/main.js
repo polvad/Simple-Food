1380bab3aeb10c776c5c14dbd641a8cbade9e98b
polvad <polvad2308@gmail.com>
2022-06-28 16:29:17 +0300

    ДЗ12

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,17 @@
-$(function(){
-  $('body').hide();
+$( function() {
+  
+  $('.header__search').on('click', function(){
+    $('.header__fild').toggleClass('active');
+  });
+
+  $('.basket__btn').on('click', function(){
+    $('.basket').toggleClass('active');
+  });
+
+  $('.basket__close').on('click', function(){
+    $('.basket').removeClass('active');
+  });
+  
+  var mixer = mixitup('.popular__category');
+
 });
--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,12 @@ const scss =          require('gulp-sass')(require('sass'));
 const concat =        require('gulp-concat');
 const autoprefixer =  require('gulp-autoprefixer');
 const uglify =        require('gulp-uglify');
+const { expr } =      require('jquery');
 const imagemin =      require('gulp-imagemin');
+const fileInclude   = require('gulp-file-include');
+const svgSprite =     require('gulp-svg-sprite');
+const cheerio =       require('gulp-cheerio');
+const replace =       require('gulp-replace');
 const del =           require('del');
 const browserSync =   require('browser-sync').create();
 
@@ -20,10 +25,12 @@ function styles() {
     }))
     .pipe(dest('app/css'))
     .pipe(browserSync.stream())
-};
+}
+
 function scripts() {
   return src([
     'node_modules/jquery/dist/jquery.js',
+    'node_modules/mixitup/dist/mixitup.js',
     'app/js/main.js'
   ])
   .pipe(concat('main.min.js'))
@@ -57,6 +64,77 @@ function images(){
   .pipe(dest('dist/images'))
 }
 
+function htmlInclude() {
+  return src(['app/html/*.html']) // Находит любой .html файл в папке "html", куда будем подключать другие .html файлы													
+  .pipe(fileInclude({
+    prefix: '@',
+    basepath: '@file',
+  }))
+  .pipe(dest('app')) // указываем, в какую папку поместить готовый файл html
+  .pipe(browserSync.stream());
+}
+
+function svgSprites() {
+  return src('app/images/icons/*.svg') // выбираем в папке с иконками все файлы с расширением svg
+    .pipe(
+      svgSprite({
+        mode: {
+          stack: {
+            sprite: '../sprite.svg', // указываем имя файла спрайта и путь
+          },
+        },
+      })
+    )
+		.pipe(dest('app/images')); // указываем, в какую папку поместить готовый файл спрайта
+}
+
+function svgSprites() {
+  return src('app/images/icons/*.svg') 
+  .pipe(cheerio({
+        run: ($) => {
+            $("[fill]").removeAttr("fill"); // очищаем цвет у иконок по умолчанию, чтобы можно было задать свой
+            $("[stroke]").removeAttr("stroke"); // очищаем, если есть лишние атрибуты строк
+            $("[style]").removeAttr("style"); // убираем внутренние стили для иконок
+        },
+        parserOptions: { xmlMode: true },
+      })
+  )
+	.pipe(
+	      svgSprite({
+	        mode: {
+	          stack: {
+	            sprite: '../sprite.svg', 
+	          },
+	        },
+	      })
+	    )
+	.pipe(dest('app/images')); 
+}
+
+function svgSprites() {
+  return src('app/images/icons/*.svg') 
+  .pipe(cheerio({
+        run: ($) => {
+            $("[fill]").removeAttr("fill"); 
+            $("[stroke]").removeAttr("stroke"); 
+            $("[style]").removeAttr("style"); 
+        },
+        parserOptions: { xmlMode: true },
+      })
+  )
+	.pipe(replace('&gt;','>')) // боремся с заменой символа 
+	.pipe(
+	      svgSprite({
+	        mode: {
+	          stack: {
+	            sprite: '../sprite.svg', 
+	          },
+	        },
+	      })
+	    )
+	.pipe(dest('app/images')); 
+}
+
 function build() {
   return src ([
     'app/**/*.html',
@@ -71,13 +149,16 @@ function cleanDist() {
 }
 
 function watching() {
+  watch(['app/html/**/*.html'], htmlInclude);
   watch(['app/scss/**/*.scss'], styles);
   watch(['app/js/**/*.js', '!app/js/mainMin.min.js'], scripts);
   watch(['app/**/*.html']).on('change', browserSync.reload);
+  watch(['app/images/icons/*.svg'], svgSprites);
 }
 
 
-
+exports.svgSprites = svgSprites;
+exports.htmlInclude = htmlInclude;
 exports.styles = styles;
 exports.scripts = scripts;
 exports.browsersync = browsersync;
@@ -86,4 +167,4 @@ exports.images = images;
 exports.cleanDist = cleanDist;
 exports.build = series(cleanDist, images, build);
 
-exports.default = parallel(styles, scripts, browsersync, watching);
+exports.default = parallel(svgSprites, htmlInclude, styles, scripts, browsersync, watching);
